4eab961c438e72bd28b491099a2c3423883d25ae
GageVanK <87737875+GageVanK@users.noreply.github.com>
2022-03-16 16:42:05 -0400

    Create Thread1.java

---
+++ Thread1.java
@@ -0,0 +1,29 @@
+public class Thread1 extends Thread{	
+	
+	
+	//Recursive Fibonacci Solution
+	public static int fib(int n)
+	{
+		
+		if (n <= 1) 
+			return n;
+		 
+		 return fib(n-1) + fib(n-2);
+	
+	
+	}
+	
+	public  void run()
+	{
+		//Calls fib() & Calculates the time it takes to run in nanoseconds
+		long startTime = (long) System.nanoTime();
+		long recursiveSolution = fib(9);
+		long endTime = (long) System.nanoTime();
+		long time = endTime - startTime;
+	
+		System.out.println("Recursive Solution: " + recursiveSolution + " in " + time + "ns");
+		
+	}
+	
+	
+}
